5f8142ae6bd892c76d20b81f36a065c0525de391
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-07-03 10:29:32 +0900

    observedAttributes / attributeChangedCallback

--- client/main.js
+++ client/main.js
@@ -1,21 +1,38 @@
-class MyElement extends HTMLElement {
+class Button extends HTMLElement {
   constructor() {
     super();
+
+    this.button = document.querySelector('button');
+
+    console.log(this.button);
   }
 
-  connectedCallback() {
-    console.log('탄생함');
+  connectedCallback() {}
+
+  disconnectedCallback() {}
+
+  static get observedAttributes() {
+    return ['id'];
   }
 
-  disconnectedCallback() {
-    console.log('죽음');
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue != newValue) {
+      //기존 값이 다르면 다시 렌더링
+      this._render();
+    }
+  }
+
+  _render() {
+    //아이디값 호출
+    this.button.textContent = this.id; //textcontent를 전달받은 this.id로 해라
   }
 }
 
-customElements.define('c-element', MyElement);
+customElements.define('c-element', Button);
 
-const elem = document.createElement('c-element');
-
-const app = document.getElementsById('app');
-
-document.body.appendChild(elem);
+const c = document.querySelector('c-button');
+//c 자체에 속성을 id의 count값을 증가하게 만들면
+let count = 0;
+c.addEventListener('click', () => {
+  c.setAttribute('id', ++count);
+});

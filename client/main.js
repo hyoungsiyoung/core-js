c2db28da69df836999690275156cfc7f95d2daba
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-07-03 13:28:20 +0900

    DOM style

--- client/main.js
+++ client/main.js
@@ -1,6 +1,27 @@
-const app = document.querySelector('#app');
-const temp = document.querySelector('#temp');
+class UserCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host{
+          background-color: orange;
+        }
+        div{
+          background:red;
+        }
+      </style>
+        <div> nickName : kind-tiger </div>
+        <slot name="username"></slot>
+        <slot name="age"></slot>
+        <slot name="gender"></slot>
+        <slot name="common"></slot>
+        <slot></slot>
+        <slot name="email">any@naver.com</slot>
 
-const clone = temp.content.cloneNode(true);
 
-app.appendChild(clone);
+    `;
+  }
+}
+
+customElements.define('user-card', UserCard);

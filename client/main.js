1316993394e2c8ec20b29f073c8a648f88f6c80a
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-07-03 11:16:18 +0900

    shadowDom

--- client/main.js
+++ client/main.js
@@ -1,6 +1,14 @@
-class Button extends HTMLElement {
+class Button extends HTMLButtonElement {
   constructor() {
     super();
+
+    //c-button의 쉐도우 돔을 열어주기
+    this.attachShadow({ mode: 'open' }); //this<-생성된 컴포넌트를 가리킴(=c-button) / mode: 'closed' 하면 닫힘
+
+    // 그리고 그 안에 (shadowRoot) 내가 원하는 태그를 넣기
+    this.shadowRoot.innerHTML = `
+      <button>hello</button>
+    `;
   }
 
   connectedCallback() {}
@@ -8,4 +16,8 @@ class Button extends HTMLElement {
   disconnectedCallback() {}
 }
 
-customElements.define('c-button', Button, { extends: 'button' });
+customElements.define('c-button', Button);
+
+console.log(
+  document.querySelector('c-button').shadowRoot.querySelector('button')
+);
